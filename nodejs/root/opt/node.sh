faedc8d66bbcc451fd2cee059e2cd363247d103f
Grayside <aross@phase2technology.com>
2015-01-19 23:18:53 -0800

    UNRW-512: Update node.sh for build.

--- nodejs/root/opt/node.sh
+++ nodejs/root/opt/node.sh
@@ -5,8 +5,14 @@ cd /var/www/html
 echo "==> Installing nodemon"
 npm install nodemon -g
 
+echo "==> Installing bower"
+npm install bower -g
+
 echo "==> Installing npm dependencies"
 npm install
+
+echo "==> Build embed assets and run tests"
+grunt
 
 echo "==> Starting the server"
 exec npm start
