69b44a7ba5707612873648a6409edecabfc0f667
Joe Turgeon <jturgeon@phase2technology.com>
2015-04-07 18:48:08 -0500

    Forking the prod env config from the current stage config.

---
+++ env/prod/opt/app/node.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd $NODE_APP_DIR
+
+echo "==> Installing bower"
+npm install bower -g
+
+echo "==> Installing npm dependencies"
+npm install --unsafe-perm
+
+echo "==> Dependency Versions"
+grunt --version
+echo "node:"
+node --version
+echo "npm:"
+npm --version
+echo "bower:"
+bower --version
+
+echo "==> Build embed assets and run tests"
+grunt
+
+echo "==> Starting the server"
+exec npm start
